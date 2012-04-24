1f3924c1ee6224b9de196fa90ccde9ae91a50ece
Evegny Mandrikov <mandrikov@gmail.com>
2012-04-24 20:29:37 +0000

    SSLRSQBR-5 Add unit test for CheckMessagesVerifierRule

---
+++ src/test/java/com/sonar/sslr/squid/checks/CheckMessagesVerifierRuleTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2010 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
+package com.sonar.sslr.squid.checks;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.sonar.squid.api.CheckMessage;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.mockito.Mockito.mock;
+
+public class CheckMessagesVerifierRuleTest {
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Test
+  public void shouldNotFailIfNothingToVerify() throws Throwable {
+    CheckMessagesVerifierRule rule = new CheckMessagesVerifierRule();
+    rule.verify();
+  }
+
+  @Test
+  public void shouldNotFailIfVerificationsWereSuccessful() throws Throwable {
+    CheckMessagesVerifierRule rule = new CheckMessagesVerifierRule();
+    rule.verify(Collections.EMPTY_LIST);
+    rule.verify(Collections.EMPTY_LIST);
+    rule.verify();
+  }
+
+  @Test
+  public void shouldFailIfFirstVerificationFailed() throws Throwable {
+    thrown.expect(AssertionError.class);
+    thrown.expectMessage("\nNo more violations expected\ngot:");
+
+    Collection<CheckMessage> messages = Arrays.asList(mock(CheckMessage.class));
+    CheckMessagesVerifierRule rule = new CheckMessagesVerifierRule();
+    rule.verify(messages);
+    rule.verify(Collections.EMPTY_LIST);
+    rule.verify();
+  }
+
+  @Test
+  public void shouldFailIfSecondVerificationFailed() throws Throwable {
+    thrown.expect(AssertionError.class);
+    thrown.expectMessage("\nNo more violations expected\ngot:");
+
+    Collection<CheckMessage> messages = Arrays.asList(mock(CheckMessage.class));
+    CheckMessagesVerifierRule rule = new CheckMessagesVerifierRule();
+    rule.verify(Collections.EMPTY_LIST);
+    rule.verify(messages);
+    rule.verify();
+  }
+
+}
